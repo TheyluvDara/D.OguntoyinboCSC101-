b8290c2624a3f910a52af0bc0b434cabf85e9424
TheyluvDara <96146034+TheyluvDara@users.noreply.github.com>
2024-10-30 16:09:00 +0100

    Practice for wk3

--- week-2/practice_1.rs
+++ week-2/practice_1.rs
@@ -7,6 +7,6 @@ fn main(){
               2. Develop some techniques in computer science,/n
               3. Understanding the different areas of study in computer science,/n
               4. Convesant with applications of computer science,/n
-              5. Navigate the career rospects in computer  science./n
+              5. Navigate the career prospects in computer  science./n
               6. Conversant with computer programming concepts.");
 }
